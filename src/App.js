a499e4f6b3f5c6e245306e074bfb0498e2965b7e
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2023-10-12 17:09:58 +0200

    premiere mise en page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="main">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='logo-container'>
+          <figure>
+            <img src='https://static.wixstatic.com/media/3f174c_08d85cf0eff844f7847191093627d821~mv2.png/v1/fill/w_230,h_163,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/LOGO%20SITE%20CLAIR.png' className="App-logo" alt="logo" />
+            <figcaption>ARCHITECTES D'INTÉRIEUR & DÉCORATEURS</figcaption>
+          </figure>
+        </div>
       </header>
+      <div id="home">
+        <aside className='nav-wrapper'>
+          <nav>
+            <ul className='internal-link'>
+              <li className='line'></li>
+              <li>PROJETS</li>
+              <li>L'AGENCE</li>
+              <li>CONTACT</li>
+              <li className='item'><span><i class="fa-brands fa-pinterest"></i></span></li>
+              <li className='item'><span><i class="fa-brands fa-instagram"></i></span></li>
+              <li className='item'><span><i class="fa-brands fa-facebook-f"></i></span></li>
+            </ul>
+          </nav>
+        </aside>
+        <section>
+        <div className='carousel-container'>
+
+        </div>
+        </section>
+      </div>
     </div>
   );
 }
