ef00f32ef01502bb0ac6c7e2731242088dafbbed
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2023-10-21 11:10:39 +0200

    add card section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
-import {useEffect, useState, useRef} from 'react';
+import {useEffect, useState } from 'react';
+import BasicCard from './components/Card';
 
 function App() {
   const [data, setData] = useState();
@@ -69,6 +70,19 @@ function App() {
             <div className='steps'>
               <h3>LES DIFFÉRENTES ÉTAPES</h3>
               <p className='steps-description'>{etapesParags}</p>
+              <div className='card-wrapper'>
+                <div className='card-container'>
+                  <BasicCard />
+                </div>
+                <div className='arrow'><span><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></span></div>
+                <div className='card-container'>
+                  <BasicCard />
+                </div>
+                <div className='arrow'><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></div>
+                <div className='card-container'>
+                  <BasicCard />
+                </div>
+              </div>
             </div>
           </article>
         </div>
