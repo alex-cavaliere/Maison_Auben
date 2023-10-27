998f1a0a097211f45da8f9f213431831f0bb8d95
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2023-10-27 18:44:35 +0200

    carousel init

---
+++ src/components/Carousel.js
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import chevronLeft from '../assets/chevron-left.svg'
+import chevronRight from '../assets/chevron-right.svg'
+
+function Carousel(props){
+    const [imgIndex, setImgIndex] = useState(0)
+    const {pictures} = props
+    const [firstPic] = useState(pictures[imgIndex])
+    //console.log(imgIndex)
+    const nextPic = () => {
+        if(imgIndex < pictures.length - 1){
+            setImgIndex(imgIndex + 1)
+        }else{
+            setImgIndex(0)
+        }
+    }
+    const prevPic = () => {
+        if(imgIndex > 0){
+            setImgIndex(imgIndex - 1)
+        }else{
+            setImgIndex(pictures.length - 1)
+        }
+    }
+    return(
+        <div className="carousel">
+            <img src={pictures[imgIndex]} alt={'photo_' + imgIndex}/>
+            {pictures.length > 1 && <div className='controls'> 
+                <div className='carousel-controls'>
+                    <img src={chevronLeft} alt='chevronLeft' onClick={prevPic}></img>
+                    <img src={chevronRight} alt='chevronRight' onClick={nextPic}></img>
+                </div>
+                <div className='carousel-counter'>{imgIndex + 1} / {pictures.length}</div>
+            </div>
+            }
+        </div>
+    )
+}
+
+export default Carousel
